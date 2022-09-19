c969c1bd5d534c941452fcb3615099b986048412
Hirisci <www.alan92@icloud.com>
2022-09-19 14:56:07 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        public string Code { get;}
+        public string Name { get ; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Iva { get; set; }
+
+
+        public Product(string name, string description, decimal price, int iva)
+        {
+            Code = FullCode();
+            Name = name;
+            Description = description;
+            Price = price;
+            Iva = iva;
+        }
+
+        private int GetRndCode()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, 10_000_000);
+        }
+
+        public string TaxPrice()
+        {
+            return String.Format("{0:F2}", Price * (1 + ((decimal)Iva / 100)));
+        }
+
+        public string FullName()
+        {
+            return $"{Code} | {Name}";
+        }
+
+        private string FullCode()
+        {
+            return GetRndCode().ToString().PadLeft( 8 , '0');
+        }
+
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using csharp_oop_shop;
+
+Product product1 = new Product("patate", "ottimi tuberi", 0.90m, 10);
+
+Console.WriteLine("Prodotto:");
+Console.WriteLine(product1.FullName());
+Console.WriteLine("Descrizzione prodotto: ");
+Console.WriteLine(product1.Description);
+Console.WriteLine("Prezzo Senza Iva: ");
+Console.WriteLine(product1.Price);
+Console.WriteLine("Prezzo Con Iva: ");
+Console.WriteLine(product1.TaxPrice());
